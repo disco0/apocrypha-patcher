9a45e315ca57f8bd567351b0dd4c56ad1c747c12
__ <disco0@users.noreply.github.com>
2023-04-16 15:17:53 -0700

    fix font-faq citation

--- res/font/MingLiUFontDataProxy.gd
+++ res/font/MingLiUFontDataProxy.gd
@@ -14,10 +14,11 @@ extends DynamicFontData
 # No, document font embedding permissions relate to embedding fonts in documents only, not embedding
 # fonts in games, apps and devices.
 #
-# > Can I embed the fonts into a game, application or device I’m developing based on the document
-# > font embedding permissions?
+# > If I convert the font into a bitmap font can I include that in my game or app?
 #
-# No, document font embedding permissions relate to embedding fonts in documents only, not embedding fonts in games, apps and devices.
+# No, converting Windows fonts to other formats does not change the rules around embedding or
+# redistribution, and format conversion itself is not allowed. Many Microsoft supplied fonts are
+# available for app and game licensing through the original font foundry or Monotype.
 
 
 const MingLiuSystemFontPath := 'C:/Windows/Fonts/mingliub.ttc'
